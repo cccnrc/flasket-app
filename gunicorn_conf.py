80157760882747ba9e8018a67c9969b39d764017
cccnrc <ec3408@cumc.columbia.edu>
2022-02-06 16:57:38 +0100

    gunicorn conf

---
+++ gunicorn_conf.py
@@ -0,0 +1,6 @@
+bind = ':7000'
+worker_class = 'sync'
+loglevel = 'debug'
+accesslog = '/var/www/flasket.net/logs/flasket_access.log'
+acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+errorlog =  '/var/www/flasket.net/logs/flasket_error.log'
